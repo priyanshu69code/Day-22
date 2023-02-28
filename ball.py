c72a84122c913fcb980f93eab6f9b31fbc0db3be
Priyanshu Kumar <jjjjyotijha10@gmail.com>
2023-02-28 20:39:22 +0530

    Impliments some logic Detecting if Right Paddel missess the ball Detecting if Left Paddel missess the Ball Detecting Collision with Paddels and Bouncing the ball

--- ball.py
+++ ball.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+from random import randint
 
 
 class Ball(Turtle):
@@ -7,9 +8,21 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
-        # self.goto()
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        new_x = self.xcor() + 1
-        new_y = self.ycor() + 1
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def reset_poss(self):
+        y_poss = randint(-190, 190)
+        self.goto(0, y_poss)
+        self.bounce_x()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from ball import Ball
 from paddel import Padel
+import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("Black")
@@ -9,9 +11,10 @@ screen.tracer(0)
 
 # Intiallizing the Ball
 ball = Ball()
-right_padel = Padel()
-left_padel = Padel()
-left_padel.goto(-380, 0)
+right_padel = Padel(350, 0)
+left_padel = Padel(-350, 0)
+scoreboard = Scoreboard()
+# left_padel.goto(-380, 0)
 screen.update()
 
 
@@ -26,5 +29,24 @@ is_gameover = True
 while is_gameover:
     ball.move()
     screen.update()
+    time.sleep(0.2)
+
+    # Detecting Collision with Up and Down Walls
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detecting Collision with Paddels and Bouncing the ball
+    if ball.distance(right_padel) < 50 and ball.xcor() > 320 or ball.distance(left_padel) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detecting if Right Paddel missess the ball
+    if ball.xcor() > 380:
+        ball.reset_poss()
+        scoreboard.l_point()
+
+    # Detecting if Left Paddel missess the Ball
+    if ball.xcor() < -380:
+        ball.reset_poss()
+        scoreboard.r_point()
 
 screen.exitonclick()
